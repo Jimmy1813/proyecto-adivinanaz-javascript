428796897dd275e63eff4ed67dca9baeda15df85
Jimmy1813 <japortilla1810@gmail.com>
2024-05-02 13:06:57 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//Se selecciona un numero al azar
+let numeroAzar = Math.floor(Math.random()*100) + 1
+
+let numeroEntrada = document.getElementById('numeroEntrada')
+let mensaje = document.getElementById('mensaje')
+let intento = document.getElementById('intento')
+let intentos = 0
+
+
+function chequearResultado(){
+    let numeroIngresado = parseInt(numeroEntrada.value)
+    intentos ++
+    intento.textContent = intentos
+    if(numeroIngresado < 1 || numeroIngresado > 100 || isNaN(numeroIngresado)){
+        mensaje.textContent = 'Por favor introduce un numero valido entre 1 y 100'
+        mensaje.style.color = 'black';
+        return
+    }
+
+    if(numeroIngresado === numeroAzar){
+        mensaje.textContent = '¡Felicitaciones! ¡Has adivinado el numero!';
+        mensaje.style.color = 'green';
+        numeroEntrada.disabled = true;
+    }else if(numeroIngresado < numeroAzar){
+        mensaje.textContent = '¡Mas alto! El numero es mayor al que dijiste' ;
+        mensaje.style.color = 'red';
+    }else {
+        mensaje.textContent = '¡Mas bajo! El numero es menor al que dijiste' ;
+        mensaje.style.color = 'red';
+    }
+}
